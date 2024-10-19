9f869c69f9cda56a04adf0a4045d203fb4b097fe
jaume <jaume.singlavalls@gmail.com>
2024-10-19 08:20:27 +0200

    Trying to get it build in the pipeline

--- create.sh
+++ create.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-flatpak-builder build-dir --force-clean es.jaumesingla.crrcsim.yaml
+flatpak-builder build-dir --force-clean com.github.dracks.crrcsim.yaml
 
-# flatpak-builder --user --install --force-clean build-dir es.jaumesingla.crrcsim.yaml
+# flatpak-builder --user --install --force-clean build-dir com.github.dracks.crrcsim.yaml
